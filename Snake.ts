ac6ac7b9652be0fdb3477ab15c4487b34c88e1ed
georgiadis.dimitri <georgiadis.dimitri34@gmail.com>
2024-10-31 13:07:17 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Snake.ts

--- Snake.ts
+++ Snake.ts
@@ -15,12 +15,43 @@ export class Snake extends Point {
         this.color = color;
       }
 
-      public move(): void {}
+      public move(): void {
+        let newHead: Point;
+    
+        switch (this.direction) {
+          case 'up':
+            newHead = new Point(this.x, this.y - 1);
+            break;
+          case 'down':
+            newHead = new Point(this.x, this.y + 1);
+            break;
+          case 'left':
+            newHead = new Point(this.x - 1, this.y);
+            break;
+          case 'right':
+          default:
+            newHead = new Point(this.x + 1, this.y);
+            break;
+        }
+    
+        this.body.unshift(new Point(this.x, this.y));
+        this.body.pop();
+        this.x = newHead.x;
+        this.y = newHead.y;
+      }
 
       public changeDirection(newDirection: string) : void {}
 
       public grow() : void {}
 
       public detectCollision() : boolean {}
+
+      public getBody(): Point[] {
+        return this.body;
+      }
+    
+      public getColor(): string {
+        return this.color;
+      }
 
     }    
